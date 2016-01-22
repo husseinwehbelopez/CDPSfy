5630f7c03c3159b3db01526ebc3339898b852971
AKQJ1Oscar <oscarvb94@gmail.com>
2016-01-22 21:35:36 +0100

    Update track.js

--- models/track.js
+++ models/track.js
@@ -9,25 +9,14 @@ track_id: {
 
 */
 
-exports.tracks = {
-	1: {
-		name: 'Cute',
-		url: '/media/Cute.mp3'
-	},
-	2: {
-		name: 'Dubstep',
-		url: '/media/Dubstep.mp3'
-	},
-	3: {
-		name: 'Epic',
-		url: '/media/Epic.mp3'
-	},
-	4: {
-		name: 'Littleidea',
-		url: '/media/Littleidea.mp3'
-	},
-	4: {
-		name: 'People',
-		url: '/media/People.ogg'
-	}
-};
+var mongoose = require('mongoose'),  
+    Schema   = mongoose.Schema;
+
+var trackSchema = new Schema({  
+	name:    { type: String },
+	url:     { type: String },
+	imgname: { type: String },
+	urlImg:  { type: String }
+});
+
+module.exports = mongoose.model('Track', trackSchema);  
